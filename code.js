0dd4eb0be1ed180b7d8247d37e39bc050126bf58
Bill <146594930+HopliteRPG@users.noreply.github.com>
2024-08-12 21:18:53 -0400

    this is a test to see if I can do the project using input

--- code.js
+++ code.js
@@ -1,10 +1,33 @@
-const myLibrary = ["john","tim"];
+const addBookBtn = document.querySelector(".add-book-btn");
 
-function Book(){
+const hobbit = new Book('The Hobbit', 'JRR Tolkein','300','read')
 
+let myLibrary = [hobbit];
+
+function Book(title,author,pages,readStatus){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
 }
 
-function addBookToLibrary(){
-    
+function addBookToLibrary(title,author,pages,readStatus){
+    title = prompt("enter title");
+    author = prompt("enter an author");
+    pages = prompt("enter the amount of pages");
+    readStatus = prompt("have you read it");
+    let addNewBook = new Book(title,author,pages,readStatus);
+    myLibrary.push(addNewBook);
+    listOutLibrary(myLibrary);
 }
 
+function listOutLibrary(library){
+    library.forEach(book => {
+        console.log(book)
+    });
+}
+
+
+
+
+listOutLibrary(myLibrary);
