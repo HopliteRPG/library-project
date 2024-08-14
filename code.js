17a87d24c40bd2c6bd884eaa4a9fb25214939872
Bill <146594930+HopliteRPG@users.noreply.github.com>
2024-08-13 21:26:23 -0400

    testing to see if modal can work for this project

--- code.js
+++ code.js
@@ -1,10 +1,52 @@
-const myLibrary = ["john","tim"];
+const addBookBtn = document.querySelector(".add-book-btn");
 
-function Book(){
+const hobbit = new Book('The Hobbit', 'JRR Tolkein','300','read')
+
+let myLibrary = [hobbit];
+
+function Book(title,author,pages,readStatus){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+}
+
+function addBookToLibrary(title,author,pages,readStatus){
 
 }
 
-function addBookToLibrary(){
-    
+function listOutLibrary(library){
+    library.forEach(book => {
+        console.log(book)
+    });
 }
 
+
+
+
+listOutLibrary(myLibrary);
+
+
+//modal test
+const dialog = document.querySelector("dialog");
+const showBtn = document.querySelector(".show-dialog-btn");
+const closeBtn = document.querySelector(".close-btn");
+const submitBtn = document.querySelector(".submit-btn");
+const bookPHolder = document.querySelector(".book_p_holder");
+const bookNameInput = document.querySelector(".book_name_input")
+
+
+
+  showBtn.addEventListener("click", () => {
+    dialog.showModal();
+  });
+
+  submitBtn.addEventListener("click", () => {
+    bookPHolder.value = favDialog.returnValue;
+
+  })
+  
+  closeBtn.addEventListener("click", () => {
+    dialog.close();
+  });
+  
