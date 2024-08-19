e3994b1fc20d0646952c3fadeb2ee35235afc37c
Bill <146594930+HopliteRPG@users.noreply.github.com>
2024-08-19 10:40:20 -0400

    create and display book to DOM  using the addBookToLibrary function

--- code.js
+++ code.js
@@ -1,8 +1,33 @@
-const addBookBtn = document.querySelector(".add-book-btn");
+//Making Book Code
 
-const hobbit = new Book('The Hobbit', 'JRR Tolkein','300','read')
+//Add delete button
+const topMainDiv = document.querySelector(".top-main-div");
 
-let myLibrary = [hobbit];
+
+const bookHtml = document.createElement("div");
+bookHtml.classList.add("book-div");
+
+const bookNameP = document.createElement("p");
+bookNameP.classList.add("bookNameP");
+
+const bookAuthorP = document.createElement("p");
+bookAuthorP.classList.add("bookAuthorP");
+
+const bookPagesP = document.createElement("p");
+bookPagesP.classList.add("bookPagesP");
+
+const bookReadP = document.createElement("p");
+bookReadP.classList.add("bookReadP");
+
+
+
+
+
+
+
+/////////////////////////
+let myLibrary = [];
+
 
 function Book(title,author,pages,readStatus){
     this.title = title;
@@ -15,7 +40,39 @@ function addBookToLibrary(title,author,pages,readStatus){
   myLibrary.push(new Book(title, author,pages,readStatus))
   listOutLibrary(myLibrary);
 
-}
+    const cloneBookHtml = bookHtml.cloneNode(true);
+  
+    const cloneBookNameP = bookNameP.cloneNode(true);
+  
+    const cloneBookAuthorP = bookAuthorP.cloneNode(true);
+  
+    const cloneBookPagesP = bookPagesP.cloneNode(true);
+  
+    const cloneBookReadP = bookReadP.cloneNode(true);
+    
+  
+  
+    topMainDiv.appendChild(cloneBookHtml);
+  
+    cloneBookHtml.appendChild(cloneBookNameP);
+  
+    cloneBookNameP.textContent = title;
+  
+    cloneBookHtml.appendChild(cloneBookAuthorP);
+  
+    cloneBookAuthorP.textContent = author;
+  
+    cloneBookHtml.appendChild(cloneBookPagesP);
+  
+    cloneBookPagesP.textContent = pages;
+  
+    cloneBookHtml.appendChild(cloneBookReadP);
+  
+    cloneBookReadP.textContent = readStatus;
+  
+  
+  }
+
 
 function listOutLibrary(library){
     library.forEach(book => {
@@ -24,18 +81,13 @@ function listOutLibrary(library){
 }
 
 
-
-
-listOutLibrary(myLibrary);
-
-
-//modal test
-const dialog = document.querySelector("dialog");
-const showBtn = document.querySelector(".show-dialog-btn");
-const output = document.querySelector("output");
-const closeBtn = document.querySelector(".close-btn");
-const submitBtn = document.querySelector(".submit-btn");
-const form = document.querySelector("form");
+//Dialong and Modal Code 
+  const dialog = document.querySelector("dialog");
+  const showBtn = document.querySelector(".show-dialog-btn");
+  const output = document.querySelector("output");
+  const closeBtn = document.querySelector(".close-btn");
+  const submitBtn = document.querySelector(".submit-btn");
+  const form = document.querySelector("form");
 
 
 
@@ -49,10 +101,9 @@ const form = document.querySelector("form");
     event.preventDefault();
     const formData = new FormData(form);
     addBookToLibrary(formData.get("book_name"),formData.get("author_name"),formData.get("pages_number"),formData.get("read_status"));
-  dialog.close();
 
-
-  })
+    dialog.close();
+  });
   
 
 
@@ -60,5 +111,7 @@ const form = document.querySelector("form");
   closeBtn.addEventListener("click", (event) => {
     event.preventDefault();
     dialog.close();
-  })
+  });
 
+
+  
